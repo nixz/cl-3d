dbfd834bea095a49c1391cdb695a33855459f217
Nikhil Shetty <nikhil.j.shetty@gmail.com>
2015-07-29 20:16:16 -0600

    Adding a window system.
    The definition of the window is taken from the following link
    
    http://doc.instantreality.org/documentation/nodetype/Window/

--- window.lisp
+++ window.lisp
@@ -33,6 +33,100 @@
 
 (in-package :cl-3d)
 
+(defclass window (xml-serializer)
+  ((enabled :initarg :enabled
+            :initform t
+            :reader enabled-changed
+            :writer set-enabled
+            :type
+            :allocation :instance
+            :documentation "If enabled is TRUE, then rendering to this window
+            is enabled")
+   (buffer :initarg :buffer
+           :initform 2
+           :allocation :instance
+           :documentation "1 (single), 2 (double), 4 (quad)")
+   (sample :initarg :sample
+           :initform 1
+           :allocation :instance
+           :documentation "number of multi-samples of FSAA")
+   (sample-filter-mode :initarg :sample-filter-mode
+           :initform "auto"
+           :reader sample-filter-mode-changed
+           :writer set-sample-filter-mode
+           :allocation :instance
+           :documentation "defines the filter-method of resolving the color of
+         multisampled pixels; hint: use the sample-field to set the number of
+         samples")
+   (position :initarg :position
+           :initform (SFVec2f -1 -1)
+           :reader position-changed
+           :writer set-position
+           :allocation :instance
+           :documentation "Position of the window on the screen. If position
+         is -1 -1, then the position is set by the window manager")
+   (size :initarg :size
+         :initform (SFVec2f 512 512)
+         :reader size-changed
+         :writer set-size
+         :allocation :instance
+         :documentation "Window size. If fullScreen is true, then window size
+         is ignored")
+   (full-screen :initarg :full-screen
+         :initform NIL
+         :reader full-screen-changed
+         :writer set-full-screen
+         :allocation  :instance
+         :documentation "If fullScreen is TRUE, the output window covers the
+         whole screen.")
+   (draw-cursor :initarg :draw-cursor
+         :initform t
+         :reader draw-cursor-changed
+         :writer set-draw-cursor
+         :allocation :instance
+         :documentation "If drawCurser is FALSE, no Cursor is shown")
+   (draw-tool-bar :initarg :draw-tool-bar
+         :initform t
+         :reader draw-tool-bar-changed
+         :writer set-draw-tool-bar
+         :allocation :instance
+         :documentation "If TRUE, the viewer tries to provide a toolbar")
+   (border :initarg :border
+         :initform t
+         :allocation :instance
+         :documentation "Show window decorations like border or title bar")
+   (pipe :initarg :pipe
+         :initform ""
+         :allocation :instance
+         :documentation "On X11 pipe is used to specify the X-Server e.g. :0
+         or rubens:1.0")
+   (description :initarg :description
+         :initform ""
+         :allocation :instance
+         :documentation "Description of the window")
+   (ignore-extensions :initarg :ignore-extensions
+         :initform ""
+         :allocation :class :instance
+         :documentation "OpenGL extensions that should be ignored during rendering")
+   (views :initarg :views
+         :initform (error ":views must be specified")
+         :writer set-views
+         :allocation :instance
+         :documentation "Viewareas that should be drawn in this window. If no
+         Viewarea isgiven, a default Viewarea is generated.")
+   (status-message :initarg :status-message
+         :initform ""
+         :writer set-status-message
+         :allocation :instance
+         :documentation "allows do set some kind of status line")
+   (visible :initarg :visible
+         :initform t
+         :writer set-visible
+         :allocation :instance
+         :documentation "Show or hide the window on the screen"))
+  (:documentation "An old fashioned window."))
+
+
 ;; ----------------------------------------------------------------------------
 (defclass opengl-window (glut:window)
   ())
